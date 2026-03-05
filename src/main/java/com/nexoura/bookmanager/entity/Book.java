be3fcd67e76240d16053adfe7da912f6ae32c4af
Kanishka <ac.kanishka@gmail.com>
2026-03-05 22:44:11 +0000

    Book Entity created

---
+++ src/main/java/com/nexoura/bookmanager/entity/Book.java
@@ -0,0 +1,24 @@
+package com.nexoura.bookmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private Double price;
+
+    private Integer publishedYear;
+}
